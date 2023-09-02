be4892d4a66ce280b1893f1814d2212f595ce50f
Babayan Armen <stavros1983@yandex.ru>
2023-09-02 09:05:19 +0400

    debug

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split()
 
